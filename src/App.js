4f099f9501fc662a0aaada7495ef016716d24ad1
Michelle031 <mbaangel2803@gmail.com>
2022-08-11 22:55:23 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Button from './components/Button';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Buttons</h1>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button />'}</p>
+          <Button />
+        </div>
+        <div>
+          <p className='small'>{'&:hover, &:focus'}</p>
+          <Button autoFocus />
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button variant="outline"/>'}</p>
+          <Button variant="outline"/>
+        </div>
+        <div>
+          <p className='small'>{'&:hover, &:focus'}</p>
+          <Button autoFocus variant="outline"/>
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button variant="text"/>'}</p>
+          <Button variant="text"/>
+        </div>
+        <div>
+          <p className='small'>{'&:hover, &:focus'}</p>
+          <Button autoFocus variant="text"/>
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button disableShadow/>'}</p>
+          <Button disableShadow color="primary" />
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button disabled/>'}</p>
+          <Button disabled />
+        </div>
+        <div>
+          <p className="label">{'<Button disabled variant="text" />'}</p>
+          <Button disabled  variant="text"/>
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button startIcon="local_grocery_store" />'}</p>
+          <Button startIcon="local_grocery_store" color="primary"/>
+        </div>
+        <div>
+          <p className="label">{'<Button endIcon="local_grocery_store" />'}</p>
+          <Button endIcon="local_grocery_store" color="primary"/>
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button size="sm" />'}</p>
+          <Button size="sm" color="primary"/>
+        </div>
+        <div>
+          <p className="label">{'<Button size="md" />'}</p>
+          <Button size="md" color="primary"/>
+        </div>
+        <div>
+          <p className="label">{'<Button size="lg" />'}</p>
+          <Button size="lg" color="primary"/>
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className="label">{'<Button color="default" />'}</p>
+          <Button color="default"/>
+        </div>
+        <div>
+          <p className="label">{'<Button color="primary" />'}</p>
+          <Button color="primary" />
+        </div>
+        <div>
+          <p className="label">{'<Button color="secondary" />'}</p>
+          <Button color="secondary"/>
+        </div>
+        <div>
+          <p className="label">{'<Button color="danger" />'}</p>
+          <Button color="danger"/>
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <p className='small'>{'&:hover, &:focus'}</p>
+          <Button autoFocus color="default"/>
+        </div>
+        <div>
+          <p className="label"></p>
+          <Button autoFocus color="primary" />
+        </div>
+        <div>
+          <p className="label"></p>
+          <Button autoFocus color="secondary"/>
+        </div>
+        <div>
+          <p className="label"></p>
+          <Button autoFocus color="danger"/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Themes.js
@@ -0,0 +1,45 @@
+const backgrounds = {
+    default: "#E0E0E0",
+    defaultHover: "#AEAEAE",
+    primary: "#2962FF",
+    primaryHover: "#0039CB",
+    secondary: "#455A64",
+    secondaryHover: "#1C313A",
+    danger: "#D32F2F",
+    dangerHover: "#9A0007",
+    
+  };
+  
+  const colors = {
+    variant: "#3D5AFE",
+    white: "#FFFFFF",
+    disabled: "#9E9E9E",
+    default: "#3F3F3F",
+  };
+
+  const sizes = {
+    sm: "6px 12px",
+    md: "8px 16px",
+    lg: "11px 22px"
+  };
+
+
+  const variants = {
+    outline: {
+        background: "transparent",
+        border: "1px solid #2962FF",
+        bgHover:  "rgba(41, 98, 255, 0.1)",
+    },
+    text: {
+        background: "transparent",
+        bgHover:  "rgba(41, 98, 255, 0.1)",
+    },
+  };
+
+  export const themes = {
+    backgrounds: backgrounds,
+    colors: colors,
+    sizes: sizes,
+    variants: variants,
+  };
+  
