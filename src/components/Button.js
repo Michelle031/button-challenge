04518fc7dc685e29f1fe8e4bc71b737b58382f1f
Michelle031 <mbaangel2803@gmail.com>
2022-08-12 16:55:28 +0100

    transition

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import {AddShoppingCart, CameraAlt, Crop, LightModeOutlined, ModeEditOutline}  from '@mui/icons-material';
+import { AddShoppingCart, CameraAlt, Crop, LightModeOutlined, ModeEditOutline } from '@mui/icons-material';
 import { themes } from "../Themes";
 
-function Button({size, variant, color, disabled, startIcon, endIcon, disableShadow, autoFocus}) {
-    const [icon, setIcon] = useState(AddShoppingCart);
-    let CustomTag = icon;
-    useEffect(() => {
-        switch (startIcon || endIcon) {
-            case "local_grocery_store":
-                setIcon(AddShoppingCart); 
-                break;
-            case "camera":
-                setIcon(CameraAlt);
-                break;
-            case "crop":
-                setIcon(Crop);
-                break;
-            case "light_mode":
-                setIcon(LightModeOutlined);
-                break;
-            case "edit":
-                setIcon(ModeEditOutline);
-                break;
-            default: 
-                return;
-        }
-    
-    }, [startIcon, endIcon]);
+function Button({ size, variant, color, disabled, startIcon, endIcon, disableShadow, autoFocus }) {
+  const [icon, setIcon] = useState(AddShoppingCart);
+  let CustomTag = icon;
+  useEffect(() => {
+    switch (startIcon || endIcon) {
+      case "local_grocery_store":
+        setIcon(AddShoppingCart);
+        break;
+      case "camera":
+        setIcon(CameraAlt);
+        break;
+      case "crop":
+        setIcon(Crop);
+        break;
+      case "light_mode":
+        setIcon(LightModeOutlined);
+        break;
+      case "edit":
+        setIcon(ModeEditOutline);
+        break;
+      default:
+        return;
+    }
+
+  }, [startIcon, endIcon]);
   return (
     <ButtonItem size={size} autoFocus variant={variant} color={color} disabled={disabled} startIcon={startIcon} endIcon={endIcon} disableShadow={disableShadow} >
-        <p> Default </p>
-        {(startIcon || endIcon) && <CustomTag sx={{height: "14px"}}/>}
+      <p> Default </p>
+      {(startIcon || endIcon) && <CustomTag sx={{ height: "14px" }} />}
     </ButtonItem>
   )
 }
@@ -44,7 +44,7 @@ const ButtonItem = styled.button`
     justify-content: center;
     width: fit-content;
     font-size: 14px;
-    background-color: ${({variant, color}) => (
+    background-color: ${({ variant, color }) => (
     (variant === "outline" && themes.variants.outline.background) ||
     (variant === "text" && themes.variants.text.background) ||
     (color === "primary" && themes.backgrounds.primary) ||
@@ -52,13 +52,14 @@ const ButtonItem = styled.button`
     (color === "danger" && themes.backgrounds.danger) ||
     (themes.backgrounds.default)
   )};
-  padding: ${({size}) => (
+  transition: background-color 0.5s ease;
+  padding: ${({ size }) => (
     (size === "sm" && themes.sizes.sm) ||
     (size === "md" && themes.sizes.md) ||
     (size === "lg" && themes.sizes.lg) ||
     (themes.sizes.md)
   )};
-  color: ${({variant, color, disabled}) => (
+  color: ${({ variant, color, disabled }) => (
     (disabled && themes.colors.disabled) ||
     (variant && themes.colors.variant) ||
     ((color === "primary" || color === "secondary" || color === "danger") && themes.colors.white) ||
@@ -68,14 +69,14 @@ const ButtonItem = styled.button`
   border-radius: 6px;
   outline: none;
   cursor: pointer;
-  box-shadow: ${({disableShadow, variant}) =>  
+  box-shadow: ${({ disableShadow, variant }) =>
     disableShadow || variant ? "" : "0px 2px 3px rgba(0, 49, 202, 0.2)"
   };
-  border: ${({variant}) =>  variant === "outline" ? themes.variants.outline.border :"none" };
-  flex-direction: ${({startIcon}) => startIcon ? "row-reverse" : "row"};
+  border: ${({ variant }) => variant === "outline" ? themes.variants.outline.border : "none"};
+  flex-direction: ${({ startIcon }) => startIcon ? "row-reverse" : "row"};
 
   :hover, :focus{
-    background-color: ${({variant, color}) => (
+    background-color: ${({ variant, color }) => (
     (variant === "outline" && themes.variants.outline.bgHover) ||
     (variant === "text" && themes.variants.text.bgHover) ||
     (color === "primary" && themes.backgrounds.primaryHover) ||
